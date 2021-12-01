5699ee069572ed1b485764f1e861f60c8ec524ba
Isac <isac.as0001@gmail.com>
2021-12-01 13:59:26 +0000

    KATA 6 - VERSIÓN 1

---
+++ src/toys/Car.java
@@ -0,0 +1,23 @@
+package toys;
+
+public class Car {
+    private Integer serialNumber;
+
+    public Car(Integer serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    
+    public void pack() {
+        System.out.printf("Packing car '%d'\n", this.serialNumber);
+    }
+    
+    public void label() {
+        System.out.printf("Labelling car '%d'\n", this.serialNumber);
+    }
+
+    public Integer getSerialNumber() {
+        return serialNumber;
+    }
+    
+    
+}
